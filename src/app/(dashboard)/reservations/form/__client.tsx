5a6b7aef1051de702c2c5e1ad4b80a42399bf56a
hckang80 <hckang80@gmail.com>
2025-08-19 18:33:55 +0900

    style: 고객정보 영역 스타일링

--- src/app/(dashboard)/reservations/form/__client.tsx
+++ src/app/(dashboard)/reservations/form/__client.tsx
@@ -1,7 +1,17 @@
 'use client';
 
-import { Button, Flex, Grid, Heading, RadioCards, TextArea, TextField } from '@radix-ui/themes';
+import {
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Heading,
+  RadioCards,
+  TextArea,
+  TextField
+} from '@radix-ui/themes';
 import { Controller, type SubmitHandler, useForm } from 'react-hook-form';
+import styles from './page.module.css';
 
 interface Client {
   koreanName: string;
@@ -64,76 +74,83 @@ export default function ReservationsFormClient() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <Heading as='h2' mb='4'>
-          고객정보
-        </Heading>
+      <Card asChild size='3'>
+        <section>
+          <Heading as='h2' mb='5'>
+            고객정보
+          </Heading>
 
-        {getValues('clients').map((client, i) => {
-          return (
-            <Grid key={client.residentId} columns='100px 1fr' gap='3'>
-              <span>이름</span>
-              <TextField.Root
-                size='3'
-                {...register(`clients.${i}.koreanName`, { required: true })}
-              />
+          <div>
+            {getValues('clients').map((client, i) => {
+              return (
+                <Grid key={client.residentId} columns='100px 1fr' gap='3' className={styles.client}>
+                  <span>이름</span>
+                  <TextField.Root
+                    size='3'
+                    {...register(`clients.${i}.koreanName`, { required: true })}
+                  />
 
-              <span>이름(영문)</span>
-              <TextField.Root
-                size='3'
-                {...register(`clients.${i}.englishName`, { required: true })}
-              />
+                  <span>이름(영문)</span>
+                  <TextField.Root
+                    size='3'
+                    {...register(`clients.${i}.englishName`, { required: true })}
+                  />
 
-              <span>성별</span>
-              <Controller
-                name={`clients.${i}.gender`}
-                control={control}
-                render={({ field }) => (
-                  <RadioCards.Root
-                    size='1'
-                    value={field.value}
-                    onValueChange={value => {
-                      field.onChange(value);
-                    }}
-                    name={field.name}
-                    columns='repeat(auto-fit, minmax(100px, auto))'
-                  >
-                    {GENDER_TYPE.map(value => (
-                      <RadioCards.Item value={value} key={value}>
-                        {value}
-                      </RadioCards.Item>
-                    ))}
-                  </RadioCards.Root>
-                )}
-              ></Controller>
+                  <span>성별</span>
+                  <Controller
+                    name={`clients.${i}.gender`}
+                    control={control}
+                    render={({ field }) => (
+                      <RadioCards.Root
+                        size='1'
+                        value={field.value}
+                        onValueChange={value => {
+                          field.onChange(value);
+                        }}
+                        name={field.name}
+                        columns='repeat(auto-fit, minmax(100px, auto))'
+                      >
+                        {GENDER_TYPE.map(value => (
+                          <RadioCards.Item value={value} key={value}>
+                            {value}
+                          </RadioCards.Item>
+                        ))}
+                      </RadioCards.Root>
+                    )}
+                  ></Controller>
 
-              <span>주민번호</span>
-              <TextField.Root
-                size='3'
-                {...register(`clients.${i}.residentId`, { required: true })}
-              />
+                  <span>주민번호</span>
+                  <TextField.Root
+                    size='3'
+                    {...register(`clients.${i}.residentId`, { required: true })}
+                  />
 
-              <span>연락처</span>
-              <TextField.Root
-                size='3'
-                {...register(`clients.${i}.phoneNumber`, { required: true })}
-              />
+                  <span>연락처</span>
+                  <TextField.Root
+                    size='3'
+                    {...register(`clients.${i}.phoneNumber`, { required: true })}
+                  />
 
-              <span>이메일</span>
-              <TextField.Root size='3' {...register(`clients.${i}.email`, { required: true })} />
+                  <span>이메일</span>
+                  <TextField.Root
+                    size='3'
+                    {...register(`clients.${i}.email`, { required: true })}
+                  />
 
-              <span>비고</span>
-              <TextArea {...register(`clients.${i}.notes`)} />
-            </Grid>
-          );
-        })}
+                  <span>비고</span>
+                  <TextArea {...register(`clients.${i}.notes`)} />
+                </Grid>
+              );
+            })}
+          </div>
 
-        <Flex justify='end' mt='4'>
-          <Button type='button' variant='surface' onClick={addClient}>
-            인원추가
-          </Button>
-        </Flex>
-      </div>
+          <Flex justify='end' mt='4'>
+            <Button type='button' variant='surface' onClick={addClient}>
+              인원추가
+            </Button>
+          </Flex>
+        </section>
+      </Card>
 
       <Flex justify='end' mt='4'>
         <Button size='3'>확인</Button>
