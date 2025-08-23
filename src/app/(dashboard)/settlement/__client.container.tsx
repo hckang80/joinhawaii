102e2dc71b7bd98e433abae7a110de344b05c903
hckang80 <hckang80@gmail.com>
2025-08-23 21:48:39 +0900

    feat: 정산 페이지 레이아웃 추가

---
+++ src/app/(dashboard)/settlement/__client.container.tsx
@@ -0,0 +1,49 @@
+import type { ReservationResponse } from '@/types';
+import { Heading, Table } from '@radix-ui/themes';
+
+export default function SettlementClientContainer({ data }: { data: ReservationResponse[] }) {
+  return (
+    <div>
+      <Heading as='h2' mb='4' size='7'>
+        예약관리
+      </Heading>
+
+      <Table.Root variant='surface' size='3'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>예약번호</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>날짜</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>예약회사</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>고객명</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>상품명</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>상품상태</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>결제상태</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>원가</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>판매가</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>환불</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>수익</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data.map(item => (
+            <Table.Row key={item.id}>
+              <Table.Cell>{item.reservation_id}</Table.Cell>
+              <Table.Cell>{item.created_at}</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+              <Table.Cell>{item.main_client_name}</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+              <Table.Cell>-</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
