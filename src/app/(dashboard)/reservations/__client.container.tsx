5185dc321d0bea6fe12566faf947da72d74951cf
hckang80 <hckang80@gmail.com>
2025-08-23 15:09:48 +0900

    chore: 데이터 출력해보기

--- src/app/(dashboard)/reservations/__client.container.tsx
+++ src/app/(dashboard)/reservations/__client.container.tsx
@@ -1,11 +1,13 @@
+import type { ReservationResponse } from '@/types';
 import { Heading } from '@radix-ui/themes';
 
-export default function ReservationsClientContainer() {
+export default function ReservationsClientContainer({ data }: { data: ReservationResponse }) {
   return (
     <div>
       <Heading as='h2' mb='4' size='7'>
         예약관리
       </Heading>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
--- src/app/(dashboard)/reservations/page.tsx
+++ src/app/(dashboard)/reservations/page.tsx
@@ -1,6 +1,7 @@
+import type { ReservationResponse } from '@/types';
 import ReservationsClientContainer from './__client.container';
 
-const fetchReservation = async (id?: string) => {
+const fetchReservation = async (id?: string): Promise<ReservationResponse> => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const url = id ? `${baseUrl}/api/reservation?reservationId=${id}` : `${baseUrl}/api/reservation`;
   const response = await fetch(url);
@@ -12,5 +13,5 @@ export default async function ReservationsPage() {
   const data = await fetchReservation();
   console.log({ data });
 
-  return <ReservationsClientContainer />;
+  return <ReservationsClientContainer data={data} />;
 }
