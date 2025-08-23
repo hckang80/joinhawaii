3f6512c1ccfa4a507455cebdaec99a5d5a15cb1d
hckang80 <hckang80@gmail.com>
2025-08-23 17:10:14 +0900

    feat: 로그아웃 핸들러 적용

--- src/app/(dashboard)/Header.tsx
+++ src/app/(dashboard)/Header.tsx
@@ -1,12 +1,26 @@
+'use client';
+
+import { createClient } from '@/utils/supabase/client';
 import { Button, Flex } from '@radix-ui/themes';
 import { User } from '@supabase/supabase-js';
 
 export default function Header({ user }: { user: User }) {
+  const supabase = createClient();
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error('Logout error:', error);
+    } else {
+      console.info('You have been logged out.');
+    }
+  };
+
   return (
     <header>
       <Flex justify='end' gap='2' p='4'>
         {user.email}
-        <Button variant='ghost' size='3'>
+        <Button variant='ghost' size='3' onClick={handleLogout}>
           logout
         </Button>
       </Flex>
