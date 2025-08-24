ea6ff25740b97d0c09cec90d8a27c9ad9b5246c5
hckang80 <hckang80@gmail.com>
2025-08-24 21:50:31 +0900

    fix: 키 오류 수정

--- src/app/api/reservation/route.ts
+++ src/app/api/reservation/route.ts
@@ -104,7 +104,7 @@ export async function GET() {
         ...hotel,
         event_date: hotel.check_in_date,
         main_client_name: reservations.main_client_name,
-        product_name: `${hotel.region} ${hotel.name} / ${hotel.room_type}`,
+        product_name: `${hotel.region} ${hotel.hotel_name} / ${hotel.room_type}`,
         type: 'hotel' as const
       })) ?? []),
       ...(tours.data?.map(({ reservations, ...tour }) => ({
