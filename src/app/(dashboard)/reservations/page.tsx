399097d45a41edcf40144e0bf8758965d80e16b6
hckang80 <hckang80@gmail.com>
2025-08-27 22:38:36 +0900

    refactor: 상품 데이터를 서버사이드로 패치

--- src/app/(dashboard)/reservations/page.tsx
+++ src/app/(dashboard)/reservations/page.tsx
@@ -1,5 +1,18 @@
+import { fetchProducts } from '@/http';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import ReservationsClientContainer from './__client.container';
 
 export default async function ReservationsPage() {
-  return <ReservationsClientContainer />;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['product', 'list'],
+    queryFn: fetchProducts
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ReservationsClientContainer />
+    </HydrationBoundary>
+  );
 }
