e9dd8a41186f000e26a4016353743e9691bec533
hckang80 <hckang80@gmail.com>
2025-08-28 12:33:43 +0900

    refactor: 코드 중복 개선

--- src/app/(dashboard)/Navigation.tsx
+++ src/app/(dashboard)/Navigation.tsx
@@ -3,37 +3,41 @@
 import { Link as StyledLink } from '@radix-ui/themes';
 import { NotebookPen, Sigma } from 'lucide-react';
 import { usePathname } from 'next/navigation';
+import { createElement } from 'react';
 import styles from './navigation.module.css';
 
 export default function Navigation() {
   const pathname = usePathname();
   const isActive = (path: string) => pathname.startsWith(path);
+  const navigation = [
+    {
+      icon: NotebookPen,
+      label: '예약관리',
+      href: '/reservations'
+    },
+    {
+      icon: Sigma,
+      label: '정산관리',
+      href: '/settlement'
+    }
+  ];
 
   return (
     <nav className={styles.nav}>
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <StyledLink
-            className={styles.link}
-            href='/reservations'
-            color='gray'
-            highContrast
-            weight={isActive('/reservations') ? 'bold' : 'regular'}
-          >
-            <NotebookPen /> 예약관리
-          </StyledLink>
-        </li>
-        <li className={styles.li}>
-          <StyledLink
-            className={styles.link}
-            href='/settlement'
-            color='gray'
-            highContrast
-            weight={isActive('/settlement') ? 'bold' : 'regular'}
-          >
-            <Sigma /> 정산관리
-          </StyledLink>
-        </li>
+        {navigation.map(({ icon, label, href }) => (
+          <li className={styles.li} key={href}>
+            <StyledLink
+              className={styles.link}
+              href={href}
+              color='gray'
+              highContrast
+              weight={isActive(href) ? 'bold' : 'regular'}
+            >
+              {createElement(icon)} {label}
+            </StyledLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
