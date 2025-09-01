4625a914403ae457f1dfb7085555505f203843d6
hckang80 <hckang80@gmail.com>
2025-09-01 20:59:02 +0900

    feat: 예약등록 링크 변경

--- src/app/(dashboard)/reservations/__client.container.tsx
+++ src/app/(dashboard)/reservations/__client.container.tsx
@@ -68,7 +68,7 @@ export default function ReservationsClientContainer() {
 
       <Flex mb='4' justify='end'>
         <Button asChild color='ruby'>
-          <Link href='/reservations/form'>예약등록</Link>
+          <Link href={`/reservations/form?from=${pathname}`}>예약등록</Link>
         </Button>
       </Flex>
 
