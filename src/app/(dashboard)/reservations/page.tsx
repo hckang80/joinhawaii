38d55c5ea0da88c6f9aafd490492c4aaeb9372bc
hckang80 <hckang80@gmail.com>
2025-09-13 01:47:16 +0900

    refactor(queryOptions): 중복 코드 제거

--- src/app/(dashboard)/reservations/page.tsx
+++ src/app/(dashboard)/reservations/page.tsx
@@ -1,15 +1,11 @@
-import { fetchProducts } from '@/http';
+import { productsQueryOptions } from '@/lib/queries';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { QUERY_KEYS } from '../../../constants';
 import ReservationsClientContainer from './__client.container';
 
 export default async function ReservationsPage() {
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: QUERY_KEYS.products.all,
-    queryFn: fetchProducts
-  });
+  await queryClient.prefetchQuery(productsQueryOptions);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
