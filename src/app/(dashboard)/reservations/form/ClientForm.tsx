57bf9a1146e0016cfbd656e3dd268a7a3b3edd67
hckang80 <hckang80@gmail.com>
2025-10-22 22:38:42 +0900

    style: 폼 레이아웃 스타일링

--- src/app/(dashboard)/reservations/form/ClientForm.tsx
+++ src/app/(dashboard)/reservations/form/ClientForm.tsx
@@ -111,185 +111,187 @@ export default function ClientForm() {
   return (
     <Card asChild size='3'>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Section>
-          <Heading as='h3' mb='4'>
-            기본정보
-          </Heading>
+        <Flex direction='column' gap='9'>
+          <Section p='0'>
+            <Heading as='h3' mb='4'>
+              기본정보
+            </Heading>
 
-          <Grid align='center' columns='60px 1fr 70px 1fr' gap='3'>
-            <Text weight='medium'>예약회사</Text>
-            <Controller
-              name='booking_platform'
-              control={control}
-              render={({ field }) => (
-                <Select.Root
-                  size='3'
-                  value={field.value}
-                  onValueChange={value => {
-                    field.onChange(value);
-                  }}
-                  name={field.name}
-                >
-                  <Select.Trigger placeholder='예약회사 선택'>{field.value}</Select.Trigger>
-                  <Select.Content>
-                    <Select.Item value='마이리얼트립'>마이리얼트립</Select.Item>
-                    <Select.Item value='크리에이트립'>크리에이트립</Select.Item>
-                    <Select.Item value='와그'>와그</Select.Item>
-                  </Select.Content>
-                </Select.Root>
-              )}
-            />
-          </Grid>
-        </Section>
+            <Grid align='center' columns='60px 1fr 70px 1fr' gap='3'>
+              <Text weight='medium'>예약회사</Text>
+              <Controller
+                name='booking_platform'
+                control={control}
+                render={({ field }) => (
+                  <Select.Root
+                    size='3'
+                    value={field.value}
+                    onValueChange={value => {
+                      field.onChange(value);
+                    }}
+                    name={field.name}
+                  >
+                    <Select.Trigger placeholder='예약회사 선택'>{field.value}</Select.Trigger>
+                    <Select.Content>
+                      <Select.Item value='마이리얼트립'>마이리얼트립</Select.Item>
+                      <Select.Item value='크리에이트립'>크리에이트립</Select.Item>
+                      <Select.Item value='와그'>와그</Select.Item>
+                    </Select.Content>
+                  </Select.Root>
+                )}
+              />
+            </Grid>
+          </Section>
 
-        <Section>
-          <Heading as='h3' mb='4'>
-            고객정보
-          </Heading>
-          {isDev() && (
-            <div>
-              합계(달러) : {data?.total_amount}
-              <br />
-              예약자: {watch('main_client_name')}
-            </div>
-          )}
+          <Section p='0'>
+            <Heading as='h3' mb='4'>
+              고객정보
+            </Heading>
+            {isDev() && (
+              <div>
+                합계(달러) : {data?.total_amount}
+                <br />
+                예약자: {watch('main_client_name')}
+              </div>
+            )}
 
-          <Table.Root size='1'>
-            <Table.Header>
-              <Table.Row>
-                <Table.ColumnHeaderCell width='60px' align='center'>
-                  예약자
-                </Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell width='80px'>이름</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell width='160px'>영문</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell width='80px'>성별</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell width='150px'>주민번호</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell width='140px'>연락처</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell width='180px'>이메일</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell>비고</Table.ColumnHeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {getValues('clients').map((client, i) => (
-                <Table.Row key={i}>
-                  <Table.Cell align='center'>
-                    <label>
-                      <Radio
-                        size='3'
-                        name='reservation'
-                        value={'' + i}
-                        defaultChecked={
-                          getValues('main_client_name')
-                            ? getValues('main_client_name') === client.korean_name
-                            : i === reservationIndex
-                        }
-                        onChange={handleChangeReservation}
-                      />
-                    </label>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Controller
-                      name={`clients.${i}.korean_name`}
-                      control={control}
-                      render={({ field }) => (
-                        <TextField.Root
-                          value={field.value}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            field.onChange(e.target.value);
-                            if (i === reservationIndex) {
-                              setValue('main_client_name', e.target.value);
-                            }
-                          }}
-                          placeholder='홍길동'
-                        />
-                      )}
-                    />
-                  </Table.Cell>
-                  <Table.Cell>
-                    <TextField.Root
-                      {...register(`clients.${i}.english_name`, { required: true })}
-                      placeholder='HONG GILDONG'
-                    />
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Controller
-                      name={`clients.${i}.gender`}
-                      control={control}
-                      render={({ field }) => (
-                        <Select.Root
-                          value={field.value}
-                          onValueChange={value => {
-                            field.onChange(value);
-                          }}
-                          name={field.name}
-                        >
-                          <Select.Trigger placeholder='성별 선택'>{field.value}</Select.Trigger>
-                          <Select.Content>
-                            {GENDER_TYPE.map(value => (
-                              <Select.Item value={value} key={value}>
-                                {value}
-                              </Select.Item>
-                            ))}
-                          </Select.Content>
-                        </Select.Root>
-                      )}
-                    />
-                  </Table.Cell>
-                  <Table.Cell>
-                    <TextField.Root
-                      {...register(`clients.${i}.resident_id`, { required: true })}
-                      placeholder='000000-0000000'
-                    />
-                  </Table.Cell>
-                  <Table.Cell>
-                    <TextField.Root
-                      {...register(`clients.${i}.phone_number`, { required: true })}
-                      placeholder='010-0000-0000'
-                    />
-                  </Table.Cell>
-                  <Table.Cell>
-                    <TextField.Root
-                      {...register(`clients.${i}.email`, { required: true })}
-                      placeholder='joinhawaii@gmail.com'
-                    />
-                  </Table.Cell>
-                  <Table.Cell>
-                    <TextField.Root {...register(`clients.${i}.notes`)} />
-                  </Table.Cell>
+            <Table.Root size='1'>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeaderCell width='60px' align='center'>
+                    예약자
+                  </Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell width='80px'>이름</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell width='160px'>영문</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell width='80px'>성별</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell width='150px'>주민번호</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell width='140px'>연락처</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell width='180px'>이메일</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>비고</Table.ColumnHeaderCell>
                 </Table.Row>
-              ))}
-            </Table.Body>
-          </Table.Root>
+              </Table.Header>
+              <Table.Body>
+                {getValues('clients').map((client, i) => (
+                  <Table.Row key={i}>
+                    <Table.Cell align='center'>
+                      <label>
+                        <Radio
+                          size='3'
+                          name='reservation'
+                          value={'' + i}
+                          defaultChecked={
+                            getValues('main_client_name')
+                              ? getValues('main_client_name') === client.korean_name
+                              : i === reservationIndex
+                          }
+                          onChange={handleChangeReservation}
+                        />
+                      </label>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Controller
+                        name={`clients.${i}.korean_name`}
+                        control={control}
+                        render={({ field }) => (
+                          <TextField.Root
+                            value={field.value}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                              field.onChange(e.target.value);
+                              if (i === reservationIndex) {
+                                setValue('main_client_name', e.target.value);
+                              }
+                            }}
+                            placeholder='홍길동'
+                          />
+                        )}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <TextField.Root
+                        {...register(`clients.${i}.english_name`, { required: true })}
+                        placeholder='HONG GILDONG'
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Controller
+                        name={`clients.${i}.gender`}
+                        control={control}
+                        render={({ field }) => (
+                          <Select.Root
+                            value={field.value}
+                            onValueChange={value => {
+                              field.onChange(value);
+                            }}
+                            name={field.name}
+                          >
+                            <Select.Trigger placeholder='성별 선택'>{field.value}</Select.Trigger>
+                            <Select.Content>
+                              {GENDER_TYPE.map(value => (
+                                <Select.Item value={value} key={value}>
+                                  {value}
+                                </Select.Item>
+                              ))}
+                            </Select.Content>
+                          </Select.Root>
+                        )}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <TextField.Root
+                        {...register(`clients.${i}.resident_id`, { required: true })}
+                        placeholder='000000-0000000'
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <TextField.Root
+                        {...register(`clients.${i}.phone_number`, { required: true })}
+                        placeholder='010-0000-0000'
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <TextField.Root
+                        {...register(`clients.${i}.email`, { required: true })}
+                        placeholder='joinhawaii@gmail.com'
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <TextField.Root {...register(`clients.${i}.notes`)} />
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
 
-          <Flex justify='end' mt='4' gap='1'>
-            <Button disabled={mutation.isPending} variant='outline'>
-              <Save />
-              변경사항 저장
-            </Button>
-            <Button
-              title='인원 삭제'
-              type='button'
-              color='ruby'
-              variant='soft'
-              onClick={() => removeItem('clients')}
-              disabled={isRemoveClientDisabled('clients')}
-            >
-              <UserMinus />
-            </Button>
-            <Button title='인원 추가' type='button' color='ruby' onClick={addClient}>
-              <UserPlus />
-            </Button>
-          </Flex>
-          {isDev() && (
-            <pre>
-              {JSON.stringify(
-                { isDirty: isDirtyProductItem('clients'), ...watch('clients') },
-                null,
-                2
-              )}
-            </pre>
-          )}
-        </Section>
+            <Flex justify='end' mt='4' gap='1'>
+              <Button disabled={mutation.isPending} variant='outline'>
+                <Save />
+                변경사항 저장
+              </Button>
+              <Button
+                title='인원 삭제'
+                type='button'
+                color='ruby'
+                variant='soft'
+                onClick={() => removeItem('clients')}
+                disabled={isRemoveClientDisabled('clients')}
+              >
+                <UserMinus />
+              </Button>
+              <Button title='인원 추가' type='button' color='ruby' onClick={addClient}>
+                <UserPlus />
+              </Button>
+            </Flex>
+            {isDev() && (
+              <pre>
+                {JSON.stringify(
+                  { isDirty: isDirtyProductItem('clients'), ...watch('clients') },
+                  null,
+                  2
+                )}
+              </pre>
+            )}
+          </Section>
+        </Flex>
       </form>
     </Card>
   );
