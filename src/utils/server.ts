5ddb48e349c1ef993714c205ed48c8ef91bbb274
hckang80 <hckang80@gmail.com>
2025-11-03 21:04:52 +0900

    feat: 유틸함수 수정

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,3 +1,5 @@
 export function getServerBaseUrl() {
-  return process.env.NEXT_PUBLIC_BASE_URL || `http://localhost:${process.env.PORT ?? 3000}`;
+  if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL.replace(/\/$/, '');
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
 }
