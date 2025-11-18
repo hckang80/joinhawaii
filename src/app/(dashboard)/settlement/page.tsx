cf02549d7ef3f55f8d0a1f3813ac8cd9871ffbad
hckang80 <hckang80@gmail.com>
2025-11-18 21:17:41 +0900

    feat: 서버사이드 하이드레이션

--- src/app/(dashboard)/settlement/page.tsx
+++ src/app/(dashboard)/settlement/page.tsx
@@ -1,13 +1,27 @@
 import { Loader } from '@/components';
+import { PER_PAGE } from '@/constants';
 import { productsQueryOptions } from '@/lib/queries';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { Suspense } from 'react';
 import SettlementClientContainer from './__client.container';
 
-export default async function SettlementPage() {
+export default async function SettlementPage({
+  searchParams
+}: {
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const params = await searchParams;
+
+  if (Array.isArray(params?.page) || Array.isArray(params?.per_page)) {
+    throw new Error('Invalid query parameters');
+  }
+
+  const page = params?.page ?? '1';
+  const perPage = params?.per_page ?? PER_PAGE;
+
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery(productsQueryOptions());
+  await queryClient.prefetchQuery(productsQueryOptions(page, perPage));
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
