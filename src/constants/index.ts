334b6e101e27399e7f52fbfdbfea5663cd697074
hckang80 <hckang80@gmail.com>
2025-12-05 14:05:34 +0900

    Reapply "feat: 성별 선택종류 업데이트"
    This reverts commit 60b57f1abfee4ebf83f298a4377edaa52157b7ce.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,7 +5,7 @@ export * from './query-keys';
 
 export const PER_PAGE = '10';
 
-export const GENDER_TYPE = ['Mr.', 'Ms.', 'Mrs.', 'Miss'] as const;
+export const GENDER_TYPE = ['MR', 'MS', 'MSTR', 'MISS'] as const;
 export type Gender = (typeof GENDER_TYPE)[number] | (string & {});
 
 export enum ProductStatus {
@@ -97,7 +97,7 @@ const additionalOptions = {
 export const defaultClientValues = {
   korean_name: '',
   english_name: '',
-  gender: 'Mr.' as Gender,
+  gender: GENDER_TYPE[0],
   resident_id: '',
   phone_number: '',
   email: '',
