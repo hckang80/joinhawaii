9a2741d46cc835062aea2da1330d87d432a848b6
hckang80 <hckang80@gmail.com>
2025-12-08 22:36:25 +0900

    refactor: 컴포넌트 내부 개선

--- src/components/DateTimeInput.tsx
+++ src/components/DateTimeInput.tsx
@@ -1,15 +1,9 @@
 'use client';
 
-import {
-  extractDateString,
-  extractTime,
-  generateHourOptions,
-  generateMinuteOptions,
-  updateDateInISO,
-  updateTimeInISO
-} from '@/utils';
-import { Flex, Select, TextField } from '@radix-ui/themes';
+import { extractDateString, updateDateInISO } from '@/utils';
+import { Flex, TextField } from '@radix-ui/themes';
 import { Controller, type Control, type FieldPath, type FieldValues } from 'react-hook-form';
+import { TimeInput } from './TimeInput';
 
 interface DateTimeInputProps<TFieldValues extends FieldValues = FieldValues> {
   name: FieldPath<TFieldValues>;
@@ -23,63 +17,26 @@ export function DateTimeInput<TFieldValues extends FieldValues = FieldValues>({
   required = false
 }: DateTimeInputProps<TFieldValues>) {
   return (
-    <Controller
-      name={name}
-      control={control}
-      rules={{ required }}
-      render={({ field }) => {
-        const dateString = extractDateString(field.value);
-        const { hours, minutes } = extractTime(field.value);
-
-        return (
-          <Flex gap='2'>
+    <Flex gap='2'>
+      <Controller
+        name={name}
+        control={control}
+        rules={{ required }}
+        render={({ field }) => {
+          const dateString = extractDateString(field.value);
+          return (
             <TextField.Root
-              {...field}
               type='date'
               value={dateString}
-              onChange={e => field.onChange(updateDateInISO(field.value, e.target.value))}
+              onChange={e => {
+                const value = e.target.value;
+                field.onChange(value ? updateDateInISO(field.value, value) : null);
+              }}
             />
-            <Flex gap='1' align='center'>
-              <Select.Root
-                value={String(hours)}
-                onValueChange={value =>
-                  field.onChange(updateTimeInISO(field.value, Number(value), minutes))
-                }
-              >
-                <Select.Trigger placeholder='시' style={{ width: '60px' }}>
-                  {String(hours).padStart(2, '0')}
-                </Select.Trigger>
-                <Select.Content>
-                  {generateHourOptions().map(hour => (
-                    <Select.Item key={hour} value={String(hour)}>
-                      {String(hour).padStart(2, '0')}
-                    </Select.Item>
-                  ))}
-                </Select.Content>
-              </Select.Root>
-              시
-              <Select.Root
-                value={String(minutes)}
-                onValueChange={value =>
-                  field.onChange(updateTimeInISO(field.value, hours, Number(value)))
-                }
-              >
-                <Select.Trigger placeholder='분' style={{ width: '60px' }}>
-                  {String(minutes).padStart(2, '0')}
-                </Select.Trigger>
-                <Select.Content>
-                  {generateMinuteOptions().map(min => (
-                    <Select.Item key={min} value={String(min)}>
-                      {String(min).padStart(2, '0')}
-                    </Select.Item>
-                  ))}
-                </Select.Content>
-              </Select.Root>
-              분
-            </Flex>
-          </Flex>
-        );
-      }}
-    />
+          );
+        }}
+      />
+      <TimeInput name={name} control={control} />
+    </Flex>
   );
 }
