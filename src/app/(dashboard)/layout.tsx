028107e3761412b661c4090fed33cb61b626188c
hckang80 <hckang80@gmail.com>
2026-02-14 16:32:58 +0900

    feat: 홈 링크 추가

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex } from '@radix-ui/themes';
 import Image from 'next/image';
+import Link from 'next/link';
 import Header from './Header';
 import styles from './layout.module.css';
 import Navigation from './Navigation';
@@ -9,7 +10,9 @@ export default async function DashboardLayout({ children }: { children: React.Re
     <Flex className={styles.root}>
       <aside className={styles.aside}>
         <h1 className={styles.logo}>
-          <Image src='/images/logo.png' width='90' height='40' alt='조인하와이' priority></Image>
+          <Link href='/'>
+            <Image src='/images/logo.png' width='90' height='40' alt='조인하와이' priority></Image>
+          </Link>
         </h1>
         <Navigation />
       </aside>
