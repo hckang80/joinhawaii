8244b268b35e4e1322aeb0ed0d687a5df7947703
hckang80 <hckang80@gmail.com>
2026-03-13 10:00:06 +0900

    feat: 상품 컴포넌트를 동적으로 추가

--- src/app/(dashboard)/reservations/form/__client.container.tsx
+++ src/app/(dashboard)/reservations/form/__client.container.tsx
@@ -118,39 +118,26 @@ export default function ReservationsFormClientContainer({
       <Flex direction='column' gap='5'>
         <ClientForm data={data} mutation={mutation} />
 
-        {isModify && <FlightForm data={data} mutation={mutation} />}
+        {isModify &&
+          PRODUCT_OPTIONS.map(opt => {
+            const Component = {
+              flight: FlightForm,
+              hotel: HotelForm,
+              tour: TourForm,
+              rental_car: RentalCarForm,
+              insurance: InsuranceForm
+            }[opt.value];
+            if (!Component) return null;
 
-        {isModify && (
-          <HotelForm
-            data={data}
-            mutation={mutation}
-            handleAdditionalOptions={handleAdditionalOptions}
-          />
-        )}
-
-        {isModify && (
-          <TourForm
-            data={data}
-            mutation={mutation}
-            handleAdditionalOptions={handleAdditionalOptions}
-          />
-        )}
-
-        {isModify && (
-          <RentalCarForm
-            data={data}
-            mutation={mutation}
-            handleAdditionalOptions={handleAdditionalOptions}
-          />
-        )}
-
-        {isModify && (
-          <InsuranceForm
-            data={data}
-            mutation={mutation}
-            handleAdditionalOptions={handleAdditionalOptions}
-          />
-        )}
+            return (
+              <Component
+                key={opt.value}
+                data={data}
+                mutation={mutation}
+                handleAdditionalOptions={handleAdditionalOptions}
+              />
+            );
+          })}
 
         {isModify && (
           <Flex justify='end' position='sticky' bottom='2' className={styles['exchange-rate-card']}>
