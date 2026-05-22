c63d43384ff9f06c43bdfb62ea867188d0a0264d
hckang80 <hckang80@gmail.com>
2026-05-22 17:44:51 +0900

    style: 예약상태 뱃지 위치 추가

--- src/app/(dashboard)/reservations/form/__client.container.tsx
+++ src/app/(dashboard)/reservations/form/__client.container.tsx
@@ -254,6 +254,15 @@ export default function ReservationsFormClientContainer({
                           )}
                         />
                       </Table.Cell>
+                      <Table.Cell rowSpan={3} style={{ borderLeft: '1px solid var(--gray-6);' }}>
+                        <Badge
+                          size='3'
+                          color={PAYMENT_STATUS_COLOR[data.payment_status]}
+                          variant='soft'
+                        >
+                          {PaymentStatus[data.payment_status]}
+                        </Badge>
+                      </Table.Cell>
                     </Table.Row>
                     <Table.Row>
                       <Table.RowHeaderCell>
