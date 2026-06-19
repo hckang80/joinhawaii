33904a129839a129ce265dcb70de4e30fb9fa1ff
hckang80 <hckang80@gmail.com>
2026-06-19 14:24:54 +0900

    style: 채널영역안내 내부에 여백 추가

--- src/app/(dashboard)/reservations/voucher/ContactInfoCards.tsx
+++ src/app/(dashboard)/reservations/voucher/ContactInfoCards.tsx
@@ -5,38 +5,42 @@ export function ContactInfoCards() {
   return (
     <Grid columns='2' gap='3' mt='4'>
       <Card>
-        <Box>
-          <Flex gap='1' align='end'>
-            <Text weight='bold'>한국</Text>
-            <Text size='1'>(09 AM - 06 PM 공휴일 휴무)</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex gap='1'>
-            <Text weight='bold'>T.</Text>
-            <Text>{CONTACT_NUMBER}</Text>
-            <Text>/</Text>
-            <Text weight='bold'>카카오톡채널.</Text>
-            <Text>조인하와이</Text>
-          </Flex>
-        </Box>
+        <Flex direction='column' gap='1'>
+          <Box>
+            <Flex gap='1' align='end'>
+              <Text weight='bold'>한국</Text>
+              <Text size='1'>(09 AM - 06 PM 공휴일 휴무)</Text>
+            </Flex>
+          </Box>
+          <Box>
+            <Flex gap='1'>
+              <Text weight='bold'>T.</Text>
+              <Text>{CONTACT_NUMBER}</Text>
+              <Text>/</Text>
+              <Text weight='bold'>카카오톡채널.</Text>
+              <Text>조인하와이</Text>
+            </Flex>
+          </Box>
+        </Flex>
       </Card>
       <Card>
-        <Box>
-          <Flex gap='1' align='end'>
-            <Text weight='bold'>하와이</Text>
-            <Text size='1'>(08 AM - 06 PM 공휴일 휴무)</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex gap='1'>
-            <Text weight='bold'>T.</Text>
-            <Text>1 808 772 2691</Text>
-            <Text>/</Text>
-            <Text weight='bold'>카카오톡ID.</Text>
-            <Text>Joinhawaiiusa</Text>
-          </Flex>
-        </Box>
+        <Flex direction='column' gap='1'>
+          <Box>
+            <Flex gap='1' align='end'>
+              <Text weight='bold'>하와이</Text>
+              <Text size='1'>(08 AM - 06 PM 공휴일 휴무)</Text>
+            </Flex>
+          </Box>
+          <Box>
+            <Flex gap='1'>
+              <Text weight='bold'>T.</Text>
+              <Text>1 808 772 2691</Text>
+              <Text>/</Text>
+              <Text weight='bold'>카카오톡ID.</Text>
+              <Text>Joinhawaiiusa</Text>
+            </Flex>
+          </Box>
+        </Flex>
       </Card>
     </Grid>
   );
