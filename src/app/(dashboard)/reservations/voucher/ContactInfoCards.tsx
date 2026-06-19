292925ece2789d7ee8be5bcffcf2a1a8e357fca8
hckang80 <hckang80@gmail.com>
2026-06-19 14:14:17 +0900

    style: 채널영역안내 수정

--- src/app/(dashboard)/reservations/voucher/ContactInfoCards.tsx
+++ src/app/(dashboard)/reservations/voucher/ContactInfoCards.tsx
@@ -1,24 +1,42 @@
 import { CONTACT_NUMBER } from '@/constants';
-import { Card, Flex, Grid, Text } from '@radix-ui/themes';
+import { Box, Card, Flex, Grid, Text } from '@radix-ui/themes';
 
 export function ContactInfoCards() {
   return (
     <Grid columns='2' gap='3' mt='5'>
       <Card>
-        <Flex direction='column' gap='1'>
-          <Text weight='bold'>조인하와이 현지 연락처</Text>
-          <Text>T : (808) 772-2691</Text>
-          <Text>카톡 : joinhawaiiusa</Text>
-          <Text>시간 : 08AM ~ 17PM</Text>
-        </Flex>
+        <Box>
+          <Flex gap='1' align='end'>
+            <Text weight='bold'>한국</Text>
+            <Text size='1'>(09 AM - 06 PM 공휴일 휴무)</Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Flex gap='1'>
+            <Text weight='bold'>T.</Text>
+            <Text>{CONTACT_NUMBER}</Text>
+            <Text>/</Text>
+            <Text weight='bold'>카카오톡채널.</Text>
+            <Text>조인하와이</Text>
+          </Flex>
+        </Box>
       </Card>
       <Card>
-        <Flex direction='column' gap='1'>
-          <Text weight='bold'>조인하와이 한국 연락처</Text>
-          <Text>T : {CONTACT_NUMBER}</Text>
-          <Text>카톡 : 조인하와이(채널)</Text>
-          <Text>시간 : 09AM ~ 18PM</Text>
-        </Flex>
+        <Box>
+          <Flex gap='1' align='end'>
+            <Text weight='bold'>하와이</Text>
+            <Text size='1'>(08 AM - 06 PM 공휴일 휴무)</Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Flex gap='1'>
+            <Text weight='bold'>T.</Text>
+            <Text>1 808 772 2691</Text>
+            <Text>/</Text>
+            <Text weight='bold'>카카오톡ID.</Text>
+            <Text>Joinhawaiiusa</Text>
+          </Flex>
+        </Box>
       </Card>
     </Grid>
   );
